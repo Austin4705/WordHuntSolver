4f60e631ba73dc85bfe8026e59b5354b5b20064a
Austin4705 <35353582+Austin4705@users.noreply.github.com>
2021-11-24 10:46:05 -0800

    dont need n wow

--- ClapSean/node.cpp
+++ ClapSean/node.cpp
@@ -4,32 +4,52 @@
 
 #include "node.h"
 
-#include <memory>
 
 //generste graph
-std::array<node, 16> node::generateGraph(const string& s){
-    for(int i = 1; i < 5; i++){
-        for(int j = 1; j < 5; j++){
-            int nodeNumber = i*j-1;
+std::array<node *, 16> node::generateGraph(const string& s){
+    assert(s.size() == 16);
+    std::array<node*, 16>  returnVal{nullptr};
+    node* arrN[4][4]{};
+    int nodeNum = 1;
+    //declare and init all the nodes
+    for(int i = 0; i < 4; i++){
+        for(int j = 1; j < 4; j++){
+            auto workingNode = new node(s[nodeNum-1], nodeNum);
+            returnVal[nodeNum-1] = workingNode;
+            arrN[i][j] = workingNode;
+            nodeNum++;
         }
     }
-    std::array<node, 16>  x;
-    return x;
+    //fill in the interconnected array of nodes
+    //indicie adj going around from 1-8, https://cdn.discordapp.com/attachments/192058549776416768/913136317100068914/unknown.png
+    std::array<std::pair<int, int>, 8> indiciesAdj = {{ {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}, {0, 1} }};
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
+            auto workingNode = arrN[i][j];
+            for(int n = 0; n < 8; n++){
+                int i1 = i+indiciesAdj[n].first, j1 = j+indiciesAdj[n].second;
+                //if in valid range set the node to the right one in the graph
+                if(i1 < 3 && i1 >= 0 && j1 < 3 && j1 >= 0){
+                    workingNode->neighborNodes[n] = arrN[i1][j1];
+                }
+            }
+        }
+    }
+    return returnVal;
 }
 
 //recursively calculate permutations, recrusive function, super inneficent and bad implementation, should be fine tho
-void node::permutations(const std::shared_ptr<std::vector<string>>& returnVal, const string& prev, node* thisN, int n, std::array<node, 16> graph){
+void node::permutations(const std::shared_ptr<std::vector<string>>& returnVal, const string& prev, node* thisN, int n, std::array<bool, 16> graph){
+    //if at end of size append to array
+    returnVal->push_back(prev);
     if(n == 1){
-        returnVal->push_back(prev+thisN->letter);
         return;
     }
     //for each eligible x node
     for(int i = 0; i < thisN->neighborNodes.size(); i++){
         if(thisN->neighborNodes[i] != nullptr){
-            if(!thisN->neighborNodes[i]->isTraveled){
-                thisN->neighborNodes[i]->isTraveled = true;
-                //I could do some like weid liked list implementaion that concatenates in o(1) but I am very lazy....,
-                // could also check with trie if there are any words larger to prevent over indexing, but I am VVVVVV lazy
+            if(!graph[thisN->neighborNodes[i]->id]){
+                graph[thisN->neighborNodes[i]->id] = true;
                 //ok did change it to be much better, still recursive and not dynamic programming but idc
                 permutations(returnVal, prev + thisN->letter, thisN->neighborNodes[i], n - 1, graph);
             }
@@ -40,23 +60,14 @@ void node::permutations(const std::shared_ptr<std::vector<string>>& returnVal, c
 //get all the permutations and add it to the list
 std::shared_ptr<std::vector<string>> node::totalPermutations(const string& c){
     //generate initial graph
-    std::array<node, 16> graph = generateGraph(c);
+    std::array<node*, 16> graph = generateGraph(c);
 
     auto returnVal = std::make_shared<std::vector<string>>();
     //for each starting node
     for(int i = 0; i < 16; i++)
-        //for each letter length
-        for(int n = 1; n <= 16; n++){
-            //copy array to a new one
-            std::array<node, 16> newGraph;
-            std::copy(graph.begin(), graph.end(), newGraph.begin());
+        //find all the permutations with that starting node nad length
+        permutations(returnVal, c, graph[i], 17, std::array<bool, 16>());
 
-            //find all the permutations with that starting node nad length
-            permutations(c, j, newGraph);
-            //copy values into return array
-            //lol so slow :)))))))
-
-        }
     return returnVal;
 }
 
--- ClapSean/node.h
+++ ClapSean/node.h
@@ -6,6 +6,8 @@
 #include <string>
 #include <array>
 #include <memory>
+#include "math.h"
+#include "assert.h"
 using std::string;
 
 
@@ -15,16 +17,24 @@ using std::string;
 //wow!! a node; so cool
 class node {
 public:
-    bool isTraveled = false;
+
+    //constructors
+    node() = default;
+    explicit node(char _letter, int _id) {
+       letter = _letter; id = _id;
+    }
+
+    //bool isTraveled = false;
     char letter = ' ';
     std::array<node*, 8> neighborNodes = {nullptr};
+    int id = -1;
 
 
     static std::shared_ptr<std::vector<string>> totalPermutations(const string& c);
     //helper functions
 private:
-    static std::array<node, 16> generateGraph(const string& s);
-    static void permutations(const std::shared_ptr<std::vector<string>>& returnVal, const string& prev, node* thisN, int n, std::array<node, 16> graph);
+    static std::array<node *, 16> generateGraph(const string& s);
+    static void permutations(const std::shared_ptr<std::vector<string>>& returnVal, const string& prev, node* thisN, int n, std::array<bool, 16> graph);
 };
 
 
